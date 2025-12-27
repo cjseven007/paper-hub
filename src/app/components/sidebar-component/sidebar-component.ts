baa87612a5d98cf8dfdce06174ac7fb3c50ca530
CJ Seven <hperm9166@gmail.com>
2025-12-27 13:44:52 +0800

    fix: Changed mobile bottom nav short label

--- src/app/components/sidebar-component/sidebar-component.ts
+++ src/app/components/sidebar-component/sidebar-component.ts
@@ -9,6 +9,7 @@ import { toSignal } from '@angular/core/rxjs-interop';
 type LucideIconName = 'house' | 'app-window' | 'settings' | 'university' | 'book-open-text';
 
 type NavItem = {
+  short_label:string;
   label: string;
   path: string;
   icon: LucideIconName;
@@ -30,9 +31,9 @@ export class SidebarComponent {
   @Input() isAdmin = false;   // <-- comes from AppComponent now
 
   nav: NavItem[] = [
-    { label: 'Home',         path: '/home',      icon: 'house' },
-    { label: 'My Workspace', path: '/workspace', icon: 'app-window' },
-    { label: 'Contribute Paper', path: '/contribute-paper', icon: 'book-open-text' },
+    { short_label: 'Home', label: 'Home',         path: '/home',      icon: 'house' },
+    { short_label: 'Workspace',label: 'My Workspace', path: '/workspace', icon: 'app-window' },
+    { short_label: 'Contribute',label: 'Contribute Paper', path: '/contribute-paper', icon: 'book-open-text' },
     // { label: 'Settings',     path: '/settings',  icon: 'settings' },
   ];
 
